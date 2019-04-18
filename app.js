0805ff00f4dff7be1ff320d8f951e4c07c831c52
uo <schoolgirluche@gmail.com>
2019-04-18 16:35:33 -0700

    Assignment 2588 completed

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ function onReady() {
     const newToDoText = document.getElementById("newToDoText");
     const toDoList = document.getElementById("toDoList");
 
-    addToDoForm.addEventListener("submit", event => {
+    addToDoForm.addEventListener("submit", (event) => {
         event.preventDefault();
 
         // get the text
         let title = newToDoText.value;
 
         // create a new li
-        let newLi = document.createElement("li");
+        let newLi = document.createElement("li");      
 
         // create a new input
         let checkbox = document.createElement("input");
@@ -18,17 +18,29 @@ function onReady() {
         // set the input's type to checkbox
         checkbox.type = "checkbox";
 
-        // set the tutle
+
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+
+
+        deleteButton.addEventListener("click", function(event) {
+            toDoList.removeChild(this.parentElement);
+        });
+
+        // set the title
         newLi.textContent= title;
 
         // attach the checkbox to the li
         newLi.appendChild(checkbox);
+
+        newLi.appendChild(deleteButton);
 
         // attach the li to the ul
         toDoList.appendChild(newLi);
 
         // empty the input
         newToDoText.value = "";
+
     });
 }
 
