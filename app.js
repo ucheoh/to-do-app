357ab94de1005536d7617ec0319bd363cf6fa643
uo <schoolgirluche@gmail.com>
2019-04-21 01:00:16 -0700

    Assignment-2589: completed

--- app.js
+++ app.js
@@ -1,46 +1,67 @@
 function onReady() {
-    const toDos = [];
-    const addToDoForm = document.getElementById("addToDoForm");
-
-    function createNewToDo() {
-        const newToDoText =document.getElementById("newToDoText");
-        if (!newToDoText.value) { return; }
-
-        toDos.push({
-            title: newToDoText.value,
-            complete: false
-        });  
-
-        newToDoText.value = ""; 
+    let toDos = [];
+    const addToDoForm = document.getElementById('addToDoForm');
+    let id = 0;
+  
+    function createNewToDo(){
+      const newToDoText = document.getElementById('newToDoText');
+      if (!newToDoText.value) return null;
+  
+      toDos.push({
+        title: newToDoText.value,
+        complete: false,
+        id: id
+      });
+  
+      id++;
+  
+      newToDoText.value = "";
+  
+      renderTheUI();
+    }
+  
+    function renderTheUI(){
+      const ToDoList = document.getElementById("toDoList");
+  
+      toDoList.textContent = "";
+  
+      toDos.forEach(function(toDo){
+        const newLi = document.createElement('li');
+        newLi.className = "newLiStyle";
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        const delBtn = document.createElement('button');
         
-        renderTheUI();
+        delBtn.className = "mdl-button mdl-js-button mdl-button--raised mdl-button--colored delBtnStyle"
+        delBtn.textContent = "Delete";
+  
+        delBtn.addEventListener('click', event => {
+          toDos = toDos.filter(function(item){
+            return item.id !== toDo.id;
+          })
+
+          renderTheUI();
+          
+        });
+  
+        newLi.textContent = toDo.title;
+  
+        toDoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+        newLi.appendChild(delBtn);
+  
+      });
     }
-
-    function renderTheUI() {
-        const toDoList = document.getElementById("toDoList");
-
-        toDoList.textContent = "";
-
-        toDos.forEach(function(toDo) {
-            const newLi = document.createElement("li");
-            const checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-
-            newLi.textContent = toDo.title;
-
-            toDoList.appendChild(newLi);
-            newLi.appendChild(checkbox);
-        });    
-    }
-
+  
     addToDoForm.addEventListener("submit", event => {
-        event.preventDefault();
-        createNewToDo();
+      event.preventDefault();
+      createNewToDo();
     });
-
     renderTheUI();
-}
-
-window.onload = function() {
+  }
+  
+  
+  window.onload = function() {
     onReady();
-};
+   };
+  
